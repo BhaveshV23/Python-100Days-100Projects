7d6cb37b5b731c96e7b8ed0d80db0f7d2c1de5cf
BhaveshV23 <bhaveshvadnere8888@gmail.com>
2026-01-10 12:06:39 +0530

    Add logic in main.py

--- Day20-21-Snake-Game/food.py
+++ Day20-21-Snake-Game/food.py
@@ -1,1 +1,21 @@
+from turtle import Turtle
+import random
+
+BOUNDARY = 280
+
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(0.5, 0.5)
+        self.color("blue")
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        x = random.randint(-BOUNDARY, BOUNDARY)
+        y = random.randint(-BOUNDARY, BOUNDARY)
+        self.goto(x, y)
 
--- Day20-21-Snake-Game/main.py
+++ Day20-21-Snake-Game/main.py
@@ -1,1 +1,56 @@
+from turtle import Screen
+import time
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+
+SCREEN_SIZE = 600
+WALL_LIMIT = 280
+SLEEP_TIME = 0.1
+
+screen = Screen()
+screen.setup(width=SCREEN_SIZE, height=SCREEN_SIZE)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(SLEEP_TIME)
+    snake.move()
+
+    # Collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Collision with wall
+    if (
+        snake.head.xcor() > WALL_LIMIT
+        or snake.head.xcor() < -WALL_LIMIT
+        or snake.head.ycor() > WALL_LIMIT
+        or snake.head.ycor() < -WALL_LIMIT
+    ):
+        scoreboard.game_over()
+        game_is_on = False
+
+    # Collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            scoreboard.game_over()
+            game_is_on = False
+
+screen.exitonclick()
 
