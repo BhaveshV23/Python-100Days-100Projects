84d52e530267b52d1d28cd0b1140eddce0941329
BhaveshV23 <bhaveshvadnere8888@gmail.com>
2026-01-22 22:07:29 +0530

    Add functionality of continuous loop

--- Day04-Rock-Paper-Scissors/main.py
+++ Day04-Rock-Paper-Scissors/main.py
@@ -1,13 +1,54 @@
 import random
-print("Welcome to Rock, Paper, and Scissors Game!")
-user_choice = int(input('Enter 1 for "rock", 2 for "paper", and 3 for "scissors"'))
-computer_choice = random.randint(1,3)
-print(computer_choice)
-if user_choice > 3 or user_choice < 1:
-    print("Wrong Input!")
-if user_choice == computer_choice:
-    print("It's a tie!")
-elif (computer_choice == 1 and user_choice == 2) or (computer_choice == 2 and user_choice == 3) or (computer_choice == 3 and user_choice == 1):
-    print("You win!")
-else:
-    print("Computer win!")
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+game_images = [rock, paper, scissors]
+
+should_continue = True
+
+while should_continue:
+    user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+    if user_choice < 0 or user_choice > 2:
+        print("You typed an invalid number, you lose!")
+    else:
+        print(game_images[user_choice])
+        computer_choice = random.randint(0,2)
+        print("Computer chose:")
+        print(game_images[computer_choice])
+
+        if user_choice == computer_choice:
+            print("It's a draw.")
+        elif (user_choice == 0 and computer_choice == 2) or (user_choice == 1 and computer_choice == 0) or (user_choice == 2 and computer_choice == 1):
+            print("You win!")
+        elif (user_choice == 0 and computer_choice == 1) or (user_choice == 1 and computer_choice == 2) or (user_choice == 3 and computer_choice == 0):
+            print("You lose!")
+
+    play_again = input("Do you want to play again? Type 'y' or 'n': ").lower()
+    if play_again == 'n':
+        should_continue = False
+        print("Goodbye!")
