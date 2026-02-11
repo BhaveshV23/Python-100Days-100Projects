88c20c994cdbf27288d44aca760cb97fb67a013e
BhaveshV23 <bhaveshvadnere8888@gmail.com>
2026-02-11 23:33:59 +0530

    refactor: upgrade birthday lookup logic and security

--- Day32-Birthday-Wisher/main.py
+++ Day32-Birthday-Wisher/main.py
@@ -2,26 +2,31 @@ from datetime import datetime
 import pandas
 import random
 import smtplib
+import os
+from dotenv import load_dotenv
 
-MY_EMAIL = "vadnerebhavesh2365@gmail.com"
-MY_PASSWORD = "oqjq gkam euwd wjmp"
+load_dotenv()
+MY_EMAIL = os.getenv("MY_EMAIL")
+MY_PASSWORD = os.getenv("MY_PASSWORD")
 
 today = datetime.now()
-today_tuple = (today.month, today.day)
+today_month = today.month
+today_day = today.day
 
 data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
-
-    with smtplib.SMTP("smtp.gmail.com") as connection:
+birthdays_today = data[(data["month"] == today_month) & (data["day"] == today_day)]
+if not birthdays_today.empty:
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
         connection.starttls()
         connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}")
+        for index, birthday_person in birthdays_today.iterrows():
+            file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    
+            with open(file_path) as letter_file:
+                contents = letter_file.read()
+                contents = contents.replace("[NAME]", birthday_person["name"])
+
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs=birthday_person["email"],
+                msg=f"Subject:Happy Birthday!\n\n{contents}")
