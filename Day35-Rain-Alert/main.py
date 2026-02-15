9281eed2fa134ff94d40e900028459851eeeabee
BhaveshV23 <bhaveshvadnere8888@gmail.com>
2026-02-15 19:22:15 +0530

    feat: add logic to get SMS from twilio

--- Day35-Rain-Alert/main.py
+++ Day35-Rain-Alert/main.py
@@ -1,5 +1,5 @@
 import requests
-import smtplib
+from twilio.rest import Client
 import os
 from dotenv import load_dotenv
 
@@ -7,11 +7,13 @@ load_dotenv()
 
 OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
 API_KEY = os.getenv("OWM_API_KEY")
+ACCOUNT_SID = os.getenv("ACCOUNT_SID")
+AUTH_TOKEN = os.getenv("AUTH_TOKEN")
+TWILIO_NUM = os.getenv("TWILIO_NUMBER")
+MY_NUM = os.getenv("MY_VERIFIED_NUMBER")
+
 MY_LAT=18.520430
 MY_LONG=73.856743
-
-MY_EMAIL = os.getenv("MY_EMAIL")
-MY_PASSWORD = os.getenv("MY_PASSWORD")
 
 weather_params = {
     "lat":MY_LAT,
@@ -24,17 +26,11 @@ response.raise_for_status()
 weather_data = response.json()
 
 if any(int(hour["weather"][0]["id"]) < 700 for hour in weather_data["list"]):
-    try:
-        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-            connection.starttls()
-            connection.login(user=MY_EMAIL, password=MY_PASSWORD)
-            connection.sendmail(
-                from_addr=MY_EMAIL,
-                to_addrs=MY_EMAIL,
-                msg="Subject: Rain Alert! ☔\n\nIt's going to rain in the next 12 hours. Grab an umbrella!"
-            )
-        print("Alert sent successfully.")
-    except smtplib.SMTPException as e:
-        print(f"Error sending email: {e}")
-else:
-    print("No rain forecast. Enjoy your day!")
+    client = Client(ACCOUNT_SID, AUTH_TOKEN)
+    message = client.messages \
+        .create(
+        body="It's going to rain today. Remember to bring an ☔",
+        from_=TWILIO_NUM,
+        to=MY_NUM
+    )
+    print(message.status)
