f82235e0a55f1d6f9909e724242e7e7663084999
BhaveshV23 <bhaveshvadnere8888@gmail.com>
2026-02-27 18:53:57 +0530

    feat: scrape Empire Online top 100 movies and save to file

---
+++ Day45-Top100-Movies/main.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
+
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+response = requests.get(URL)
+response.raise_for_status()
+website_html = response.text
+
+soup = BeautifulSoup(website_html, "html.parser")
+all_movies = soup.find_all(name="h3", class_="title")
+
+movie_titles = [movie.getText() for movie in all_movies]
+movies = movie_titles[::-1]
+
+with open("Day45-Top100-Movies/movies.txt", mode="w", encoding="utf-8") as file:
+    for movie in movies:
+        file.write(movie + "\n")
+
+print("Top 100 movies saved successfully!")
