4f8cdc6fcd75e403bcbe04e2bd0fc3e642739217
BhaveshV23 <bhaveshvadnere8888@gmail.com>
2026-03-04 23:28:05 +0530

    refactor: used WebDriverWait for timeout.

--- Day48-Cookie-Clicker-Bot/main.py
+++ Day48-Cookie-Clicker-Bot/main.py
@@ -1,66 +1,70 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from time import sleep, time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
+from time import time, sleep
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(options=chrome_options)
 
+driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://ozh.github.io/cookieclicker/")
 
-sleep(3)
+wait = WebDriverWait(driver, 15)
 
 print("Looking for language selection...")
 try:
-    language_button = driver.find_element(by=By.ID, value="langSelect-EN")
-    print("Found language button, clicking...")
+    language_button = wait.until(
+        EC.element_to_be_clickable((By.ID, "langSelect-EN"))
+    )
     language_button.click()
-    sleep(3)
-except NoSuchElementException:
+    print("English language selected")
+except:
     print("Language selection not found")
 
-sleep(2)
+cookie = wait.until(
+    EC.element_to_be_clickable((By.ID, "bigCookie"))
+)
 
-cookie = driver.find_element(by=By.ID, value="bigCookie")
-# cookie.click()
+upgrade_check_interval = 5
+next_upgrade_check = time() + upgrade_check_interval
 
-item_ids = [f"product{i}" for i in range(18)]
-
-wait_time = 5
-timeout = time() + wait_time
-five_min = time() + 60 * 5
+game_end_time = time() + 60 * 5 
 
 while True:
+
     cookie.click()
 
-    if time() > timeout:
+    sleep(0.01)
+
+    if time() > next_upgrade_check:
+
         try:
-            cookies_element = driver.find_element(by=By.ID, value="cookies")
-            cookies_text = cookies_element.text
-            cookies_count = int(cookies_text.split()[0].replace(",",""))
+            products = driver.find_elements(By.CSS_SELECTOR, "div[id^='product']")
 
-            products = driver.find_elements(by=By.CSS_SELECTOR, value="div[id^='product']")
+            best_product = None
 
-            best_item = None
             for product in reversed(products):
                 if "enabled" in product.get_attribute("class"):
-                    best_item = product
+                    best_product = product
                     break
 
-            if best_item:
-                best_item.click()
-                print(f"Bought item: {best_item.get_attribute('id')}")
+            if best_product:
+                best_product.click()
+                print(f"Bought: {best_product.get_attribute('id')}")
 
-        except (NoSuchElementException, ValueError):
-            print("Couldn't find cookie count or items")
-
-        timeout = time() + wait_time
-
-    if time() > five_min:
-        try:
-            cookies_element = driver.find_element(by=By.ID, value="cookies")
-            print(f"Final result: {cookies_element.text}")
         except NoSuchElementException:
-            print("Couldn't get final cookie count")
+            print("Upgrade not found")
+
+        next_upgrade_check = time() + upgrade_check_interval
+
+    if time() > game_end_time:
+
+        try:
+            cookies = driver.find_element(By.ID, "cookies").text
+            print(f"\nFinal Result: {cookies}")
+        except NoSuchElementException:
+            print("Could not fetch final score")
+
         break
